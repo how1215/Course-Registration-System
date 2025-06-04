3bcfbf434052499f7cebee768e78b6433f17522e
how1215 <nick0973299916@gmail.com>
2025-06-04 13:54:39 +0800

    MM

--- system.cpp
+++ system.cpp
@@ -2,18 +2,39 @@
 #include <iostream>
 #include <vector>
 
+class Student{
+        private:
+        std::string name;
+        int id;
+        std::vector <CourseSection> courses;
 
-void thread_function(int i){
-    std::cout << "I am thread" << i  << std::endl;
-}
-s
+    public:
+        Student(std::string name, int id):name(name),id(id){}
+        
+};
+
+// class CourseSection{
+
+
+
+// };
+
+
+
+
 int main(){
     std::vector<std::thread> threads;
-    for(int i = 0; i < 3; i++){
-        threads.push_back(std::thread(thread_function, i));
+    std::vector<Student> students={
+        Student("Nick", 111),
+        Student("Jane", 222),
+        Student("Jim", 333)
+    };
+
+    for (auto& s : students) {
+        threads.emplace_back(std::thread(std::ref(s)));
     }
-    for(auto& thread : threads){
-        thread.join();
+    for(auto& t : threads){
+        t.join();
     }
 
     std::cout << "I am dick thread and I am about to finish"<< std::endl;
