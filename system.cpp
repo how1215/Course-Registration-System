6608a07f2faa004984fc61753eeb6ff55242d15e
NIck <nick@NIckdeMacBook-Pro.local>
2025-06-04 11:16:02 +0800

    D

--- system.cpp
+++ system.cpp
@@ -6,10 +6,9 @@
 void thread_function(int i){
     std::cout << "I am thread" << i  << std::endl;
 }
-
+s
 int main(){
     std::vector<std::thread> threads;
-
     for(int i = 0; i < 3; i++){
         threads.push_back(std::thread(thread_function, i));
     }
@@ -17,6 +16,6 @@ int main(){
         thread.join();
     }
 
-    std::cout << "I am master thread and I am about to finish"<< std::endl;
+    std::cout << "I am dick thread and I am about to finish"<< std::endl;
     return 0;
 }
