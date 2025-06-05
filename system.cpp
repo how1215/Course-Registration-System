be0f0facc209e513fef0430d00895185b71ce4b8
how1215 <nick0973299916@gmail.com>
2025-06-05 23:19:17 +0800

    Auto update from git-sync.sh

--- system.cpp
+++ system.cpp
@@ -15,17 +15,12 @@ class Student{
     public:
         Student(std::string name, int id):name(name),id(id){}
 
-        void increment(){
-            std::lock_guard<std::mutex> lock(mtx);
-            shared_count++;  // 所有執行緒共同增加這個數字
-            std::cout << "Student " << name << " (ID: " << id << ") incremented shared_count to: " << shared_count << std::endl;
-        }
         
 };
 
 class CourseSection{
     private:
-        
+
 
 
 };
@@ -41,14 +36,6 @@ int main(){
         Student("Jim", 333)
     };
 
-    for (auto& s : students) {
-        threads.push_back(std::thread(&Student::increment, std::ref(s)));
-    }
-    for(auto& t : threads){
-        t.join();
-    }
 
-    std::cout << "Final shared_count value: " << shared_count << std::endl;
-    std::cout << "I am master thread and I am about to finish"<< std::endl;
     return 0;
 }
